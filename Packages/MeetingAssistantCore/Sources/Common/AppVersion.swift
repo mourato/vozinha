50409d3769e0b6c48b6beb1227c382978eb4fbfe
Renato <remourato@gmail.com>
2026-08-04 20:41:44 -0300

    refactor(recording): remove redundant dictation coordinator
    - Deleted IncrementalDictationTranscriptionCoordinator as it is no longer required.
    - Renamed IncrementalMeetingTranscriptionCoordinator to IncrementalTranscriptionCoordinator to simplify the naming convention.

--- Packages/MeetingAssistantCore/Sources/Common/AppVersion.swift
+++ Packages/MeetingAssistantCore/Sources/Common/AppVersion.swift
@@ -15,10 +15,10 @@ public enum AppVersion {
     // MARK: - Hardcoded Constants (Update these when releasing)
 
     /// Hardcoded version string - update this when releasing a new version
-    private static let hardcodedVersion = "0.7.25"
+    private static let hardcodedVersion = "0.8.4"
 
     /// Hardcoded build number - update this when creating a new build
-    private static let hardcodedBuild = "93"
+    private static let hardcodedBuild = "94"
 
     // MARK: - Public API
 
